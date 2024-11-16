e7c626868c8eb9f9ebfdaecae6728a75f07bb3da
Matheus Gomes <matheusgomes192@hotmail.com>
2024-11-16 11:48:41 -0300

    Criação de Input com conexão a API de localização do Google para autocomplete + Uso da API de Geolocalização também do Google para pegar as coordenadas e atualizar no mapa em tempo real

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import MapComponent from './components/MapComponent';
 const App: React.FC = () => {
   return (
     <div className="App" data-testid="map-container">
-      <h1>Mapa com Leaflet no React</h1>
       <MapComponent />
     </div>
   );
--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,13 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 // import iconUrl from 'leaflet/dist/images/marker-icon.png';
 // import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 // import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+interface Coordinates {
+  lat: number | null;
+  lng: number | null;
+}
 
 const MapComponent: React.FC = () => {
+  const [address, setAddress] = useState<string>("São Paulo, SP, Brasil");
+  const [coordinates, setCoordinates] = useState<Coordinates>({ lat: -23.550520, lng: -46.633308 });
+
   const mapRef = useRef<HTMLDivElement>(null);
+
+  const handleSelect = async (value: string) => {
+    setAddress(value);
+    try {
+      const results = await geocodeByAddress(value);
+      const latLng = await getLatLng(results[0]);
+      setCoordinates(latLng);
+      console.log(latLng);
+    } catch (error) {
+      console.error("Erro ao obter as coordenadas:", error);
+    }
+  };
 
   useEffect(() => {
     if (!mapRef.current) return;
@@ -27,7 +47,7 @@ const MapComponent: React.FC = () => {
     L.Marker.prototype.options.icon = defaultIcon;
 
     // Inicializa o mapa com posição inicial e zoom
-    const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
+    const map = L.map(mapRef.current).setView([coordinates.lat, coordinates.lng], 13);
 
     // Adiciona uma camada de tiles do OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -35,16 +55,52 @@ const MapComponent: React.FC = () => {
     }).addTo(map);
 
     // Adiciona um marcador e popup
-    const marker = L.marker([51.505, -0.09]).addTo(map);
-    marker.bindPopup('Aqui é Londres! <br> Exemplo de Popup.');
+    const marker = L.marker([coordinates.lat, coordinates.lng]).addTo(map);
+    marker.bindPopup('Aqui é '+address+'!');
 
     // Limpa o mapa ao desmontar o componente
     return () => {
       map.remove();
     };
-  }, []);
+  }, [coordinates]);
 
-  return <div ref={mapRef} data-testid="map-container" style={{ height: '500px', width: '100%' }} />;
+  return (
+    <div className="flex flex-col h-[100vh] items-center bg-[#121212] justify-center">
+      <div className="max-w-[500px] w-[100%] mx-auto mt-10 p-6 bg-[#1f1f1f] shadow-lg rounded-lg">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4 text-center text-[#e0e0e0]">Search Location</h2>
+        <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
+          {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+            <div className="relative">
+              <input
+                {...getInputProps({ placeholder: "Type a location..." })}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#6200ea]"
+              />
+              <div className="absolute w-full bg-[#1f1f1f] text-white rounded-lg mt-1 z-10">
+                {suggestions.map((suggestion) => {
+                  const activeClass = suggestion.active
+                    ? "bg-[#6200ea] cursor-pointer"
+                    : "bg-[#1f1f1f] cursor-pointer";
+                  return (
+                    <div
+                      key={suggestion.placeId}
+                      {...getSuggestionItemProps(suggestion, {
+                        className: `p-2 ${activeClass} hover:bg-[#6200ea]`,
+                      })}
+                    >
+                      {suggestion.description}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      </div>
+
+      <div ref={mapRef} data-testid="map-container" className="h-[500px] w-[90%] mt-5 z-0" />
+    </div>
+
+  );
 };
 
 export default MapComponent;
